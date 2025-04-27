5033603fd0dd97c692ee3b23c505e39ccfd30fde
Gerard Ike <chinedumike85@gmail.com>
2025-04-27 16:23:23 -0400

    build errors

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,18 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    ...compat.extends("next/core-web-vitals", "next/typescript"),
+    "rules": {
+      "@typescript-eslint/ban-ts-comment": [
+        "error",
+        {
+          "ts-nocheck": false 
+        }
+      ]
+    }
+  }
+  
 ];
 
 export default eslintConfig;
