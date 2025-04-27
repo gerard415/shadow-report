7040d659103fb0dc0ffa57d15483a28ce3877dfb
Gerard Ike <chinedumike85@gmail.com>
2025-04-27 14:59:42 -0400

    completing the auth

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
+import { Providers } from "./providers";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -26,7 +27,9 @@ export default function RootLayout({
           </div>
           <Navbar />
           <main className="pt-16">
-            {children}
+            <Providers>
+              {children}
+            </Providers>
           </main>
         </div>
       </body>
